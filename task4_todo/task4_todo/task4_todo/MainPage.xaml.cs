fb78cb4b395a1e7ad35719baa6678350e11d3fc1
hadeefathima <hadeefathima10@gmail.com>
2017-12-06 23:21:33 +0530

    Task4

---
+++ task4_todo/task4_todo/task4_todo/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace task4_todo
+{
+	public partial class MainPage : ContentPage
+	{
+        List<string> member;
+        public MainPage()
+		{
+			InitializeComponent();
+            
+        }
+        public void View(object o, EventArgs e)
+        {
+            member = new List<string>();
+
+            member.Add(item1.Text);
+            member.Add(item2.Text);
+            member.Add(item3.Text);
+            member.Add(item4.Text);
+
+             Navigation.PushAsync(new ViewPage(member));
+        }
+    }
+}
---
+++ task4_todo/task4_todo/task4_todo/ViewPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace task4_todo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ViewPage : ContentPage
+	{
+		public ViewPage (List<string> mem)
+		{
+			InitializeComponent ();
+            listview.ItemsSource = mem;
+		}
+	}
+}
