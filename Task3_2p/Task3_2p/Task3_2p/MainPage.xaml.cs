9bbdfc7be4e3539db040a4ccaa637fe80288a83d
hadeefathima <hadeefathima10@gmail.com>
2017-12-06 21:34:40 +0530

    Task3

---
+++ Task3_2p/Task3_2p/Task3_2p/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Task3_2p
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private async void submit(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new ViewPage(n.Text,em.Text,p.Text));
+        }
+
+    }
+}
---
+++ Task3_2p/Task3_2p/Task3_2p/ViewPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Task3_2p
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ViewPage : ContentPage
+	{
+		public ViewPage (String name,String email,String prof)
+		{
+			InitializeComponent ();
+            nm.Text = name;
+            el.Text = email;
+            pf.Text = prof;
+		}
+	}
+}
