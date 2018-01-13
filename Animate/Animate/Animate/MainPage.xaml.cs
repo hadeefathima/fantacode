2823b761d4b9ab0af00c2ad666e5c18a6f8e075f
hadeefathima <34306304+hadeefathima@users.noreply.github.com>
2018-01-13 18:51:13 +0530

    Animate

---
+++ Animate/Animate/Animate/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Animate
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        public async void move(object sender, EventArgs e)
+        {
+            await mans.TranslateTo(300, 0, 1000, Easing.Linear);
+           // await man.TranslateTo(0, 300, 1000, Easing.Linear);
+
+
+        }
+        public async void down(object sender, EventArgs e)
+        {
+           // await man.TranslateTo(300, 0, 1000, Easing.Linear);
+            await mans.TranslateTo(300, 300, 1000);
+            await mans.TranslateTo(0, 300, 1000);
+            await mans.TranslateTo(0, 0, 1000);
+            await mans.TranslateTo(150, 150, 1000, Easing.Linear);
+            await mans.ScaleTo(5, 1000);
+
+
+        }
+
+    }
+}
