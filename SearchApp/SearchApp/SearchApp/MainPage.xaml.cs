069f682f0fbffee6f206b8beb6e2a7d6fef50e35
hadeefathima <34306304+hadeefathima@users.noreply.github.com>
2018-01-30 18:39:50 +0530

    week9

---
+++ SearchApp/SearchApp/SearchApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SearchApp
+{
+	public partial class MainPage : ContentPage
+	{
+        private readonly List<string> _names = new List<string>
+        {
+            "Adnan","Adil","Lulu","jasna","fida","husna"
+        };
+		public MainPage()
+		{
+			InitializeComponent();
+            MainList.ItemsSource = _names;
+		}
+        private void searchbut(object sender,EventArgs e)
+        {
+            string keyword = MainSearch.Text;
+            IEnumerable<string> searchResult = _names.Where(name => name.Contains(keyword));
+            MainList.ItemsSource = searchResult;
+        }
+	}
+}
